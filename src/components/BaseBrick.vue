03842824d3f67b8acb4ba546fbc0d5626c9efda3
Bartelix <bartlomiej.bartoszewicz95@gmail.com>
2022-04-11 21:30:14 +0200

    BaseBrick, HorizontalPadleBrick Fix typo in 'scoped' word Standarized Bricks components

--- src/components/BaseBrick.vue
+++ src/components/BaseBrick.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="brick-element"></div>
+  <div class="brick">
+    <div class="brick-element"></div>
+  </div>
 </template>
 
 <script>
@@ -8,13 +10,19 @@ export default {
 };
 </script>
 
-<style scope lang="scss">
+<style scoped lang="scss">
 $brick-element-size: clamp(3rem, 10vmin, 5rem);
+
+.brick {
+  display: grid;
+  gap: 0.125rem;
+  padding: 0.125rem;
+  background-color: rgba(0, 0, 0, 0.7);
+}
 
 .brick-element {
   width: $brick-element-size;
   height: $brick-element-size;
   background-color: rgb(121, 182, 242);
-  border: 1px solid #fff;
 }
 </style>
--- src/components/HorizontalPadleBrick.vue
+++ src/components/HorizontalPadleBrick.vue
@@ -12,17 +12,20 @@ export default {
 };
 </script>
 
-<style scope lang="scss">
+<style scoped lang="scss">
 $brick-element-size: clamp(3rem, 10vmin, 5rem);
 
 .brick {
-  display: flex;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 0.125rem;
+  padding: 0.125rem;
+  background-color: rgba(0, 0, 0, 0.7);
 }
 
 .brick-element {
   width: $brick-element-size;
   height: $brick-element-size;
   background-color: rgb(121, 182, 242);
-  border: 1px solid #fff;
 }
 </style>
