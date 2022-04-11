97bb1ffac9dcd58a45a16f4cf2d09e72840ac798
Bartelix <bartlomiej.bartoszewicz95@gmail.com>
2022-04-11 21:29:25 +0200

    GameBoard - display number in cell if it is greater than 1

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="game-board">
     <div class="game-board-cells">
-      <div v-for="(cell, i) in getCells" :key="i" :class="cellClasses(i)"></div>
+      <div v-for="(cell, i) in getCells" :key="i" :class="cellClasses(i, cell)">
+        {{ cell > 1 ? cell : "" }}
+      </div>
     </div>
   </div>
 </template>
@@ -19,11 +21,13 @@ export default {
   },
   computed: {
     getCells() {
-      return cells(this.size * this.size);
+      return cells(this.size * this.size).map((_, i) =>
+        i < 3 ? 2 : i < 6 ? 1 : 0
+      );
     },
   },
   methods: {
-    cellClasses(i) {
+    cellClasses(i, cell) {
       const r = Math.floor(i / this.size);
       const c = i % this.size;
 
@@ -31,7 +35,9 @@ export default {
         ((r <= 2 || r >= 6) && (c <= 2 || c >= 6)) ||
         (r >= 3 && r <= 5 && c >= 3 && c <= 5);
 
-      return `cell cell--${isGray ? "gray" : "white"}`;
+      return `cell cell--${isGray ? "gray" : "white"} ${
+        cell > 0 ? "cell--brick" : ""
+      }`;
     },
   },
 };
@@ -54,6 +60,11 @@ $cell-size: clamp(3rem, 10vmin, 5rem);
 .cell {
   width: $cell-size;
   height: $cell-size;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 3rem;
+  font-weight: bold;
 
   &.cell--white {
     background-color: #fff;
@@ -61,6 +72,11 @@ $cell-size: clamp(3rem, 10vmin, 5rem);
 
   &.cell--gray {
     background-color: #ddd;
+  }
+
+  &.cell--brick {
+    background-color: rgb(121, 182, 242);
+    color: #fff;
   }
 }
 </style>
